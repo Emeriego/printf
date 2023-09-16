618c99c8f9954a53201a854f9c70b150b5ef61c2
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-16 15:16:12 +0100

    my costum _prinf function

--- src/play.h
+++ src/play.h
@@ -5,20 +5,22 @@ int _printf(const char *format, ...)
     va_list args;
     va_start(args, format);
 
-    int printed_chars = 0;  // To keep track of the number of characters printed
+    int printed_chars; 
+    printed_chars = 0;
+     char specifier;
 
     while (*format)
     {
         if (*format == '%')
         {
-            format++;  // Move past '%'
-            char specifier = *format;
+            format++;  
+            specifier = *format;
             switch (specifier)
             {
                 case 'c':
                 {
-                    char c = va_arg(args, int);  // 'c' is promoted to int
-                    write(1, &c, 1);  // Print the character
+                    char c = va_arg(args, int); 
+                    write(1, &c, 1); 
                     printed_chars++;
                     break;
                 }
@@ -28,24 +30,24 @@ int _printf(const char *format, ...)
                     int len = 0;
                     while (str[len])
                         len++;
-                    write(1, str, len);  // Print the string
+                    write(1, str, len); 
                     printed_chars += len;
                     break;
                 }
                 case '%':
-                    write(1, "%", 1);  // Print a single '%'
+                    write(1, "%", 1); 
                     printed_chars++;
                     break;
                 default:
-                    break;  // Handle other format specifiers as needed
+                    break;  
             }
         }
         else
         {
-            write(1, format, 1);  // Print non-format specifier characters
+            write(1, format, 1);  
             printed_chars++;
         }
-        format++;  // Move to the next character in the format string
+        format++;  
     }
 
     va_end(args);
---
+++ src/test.h
@@ -0,0 +1,10 @@
+#include "main.h" 
+
+
+int main(void)
+{
+     _printf("Hello, %s!\n", "world");
+    printf("Hello, %s!\n", "world");
+
+    return (0);
+}
