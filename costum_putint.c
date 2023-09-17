14355cf017e972b955d7ba9d2be18b19e0fe25fc
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-17 02:14:26 +0100

    task 0

--- costum_putint.c
+++ costum_putint.c
@@ -8,19 +8,17 @@
  */
 int custom_putint(int num)
 {
-    int character_count = 0;
+	int character_count = 0;
 
-    if (num < 0)
-    {
-        character_count += put_char('-');
-        num = -num;
-    }
-
-    if (num / 10 != 0)
-    {
-        character_count += custom_putint(num / 10);
-    }
-    character_count += put_char('0' + (num % 10));
-
-    return character_count;
+	if (num < 0)
+	{
+		character_count += put_char('-');
+		num = -num;
+	}
+	if (num / 10 != 0)
+	{
+		character_count += custom_putint(num / 10);
+	}
+	character_count += put_char('0' + (num % 10));
+	return (character_count);
 }
