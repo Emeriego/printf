27ff4fae083158ff1646564411b94c646b7085cb
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-17 02:10:18 +0100

    task 0

--- _printf.c
+++ _printf.c
@@ -6,55 +6,47 @@
  * @format: The format string
  * Return: The number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-     int character_count, m, num;
-     char c, *str;
-     va_list my_args;
-    character_count = 0;  
-    
+	int character_count, m, num;
+	char c, *str;
+	va_list my_args;
 
-    if (!format || (format[0] == '%' && format[1] == '\0'))
-    {
-          return (-1);
-    }
-       
-    va_start(my_args, format);
-
-    for (m = 0; format[m] != '\0'; m++)
-    {
-        if (format[m] != '%')
-        {
-            put_char(format[m]);
-        }
-        else if (format[m] == '%' && format[m + 1] == 'c')
-        {
-            c = va_arg(my_args, int);
-            put_char(c); 
-            m++; 
-        }
-        else if (format[m] == '%' && format[m + 1] == 's')
-        {
-            str = va_arg(my_args, char *);
-            character_count += put_str(str); 
-            m++; 
-        }
-        else if (format[m] == '%' && format[m + 1] == '%')
-        {
-            put_char('%'); 
-        }
-        else if (format[m + 1] == 'd' || format[m + 1] == 'i')
-        {
-            num = va_arg(my_args, int);
-            character_count += custom_putint(num);
-            m++;
-        }
-        else
-        {
-            put_char('%');
-        }
-        character_count++;
-    }
-    va_end(my_args);
-    return character_count;
+	character_count = 0;
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(my_args, format);
+	for (m = 0; format[m] != '\0'; m++)
+	{
+		if (format[m] != '%')
+		{
+			put_char(format[m]);
+		}
+		else if (format[m] == '%' && format[m + 1] == 'c')
+		{
+			c = va_arg(my_args, int);
+			put_char(c);
+			m++;
+		}
+		else if (format[m] == '%' && format[m + 1] == 's')
+		{
+			str = va_arg(my_args, char *);
+			character_count += put_str(str);
+			m++;
+		}
+		else if (format[m] == '%' && format[m + 1] == '%')
+			put_char('%');
+		else if (format[m + 1] == 'd' || format[m + 1] == 'i')
+		{
+			num = va_arg(my_args, int);
+			character_count += custom_putint(num);
+			m++;
+		}
+		else
+		put_char('%');
+		character_count++;
+	}
+	va_end(my_args);
+	return (character_count);
 }
