c851cbe988fe56fab6a06c032b010b04104b1653
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-17 10:17:19 +0100

    task 0

--- put_char.c
+++ put_char.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_char - Print a character
+ * put_char - Print a character
  * @args: The argument list
  *
  * Return: 1 if successful
  */
-int print_char(va_list my_args)
+int put_char(va_list my_args)
 {
     char character = va_arg(my_args, int);
     _putchar(character);
