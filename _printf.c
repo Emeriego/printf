e2e01118e48a211c5ae3c034c78b84c149c60931
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-18 10:16:06 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@
 int _printf(const char * const format, ...)
 {
 	c_specifier spec[] = {
-		{"s", put_string}, {"c", put_character},
+		{"%s", put_string}, {"%c", put_character},
 		{"%i", put_int}, {"%d", put_dec}, {"%r", put_str_rev},
 		{"%R", put_r13}, {"%b", put_bin}, {"%u", put_unsign},
 		{"%o", put_oct}, {"%x", put_hex2}, {"%X", put_hex},
