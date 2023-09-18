b5634a73b0be5fe000514de4996cdaf6da4a81c0
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-18 12:54:54 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	c_specifier spec[] = {
 		{"%s", put_string}, {"%c", put_character},
