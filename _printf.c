b74e53982f8f133ba23091e288092bce29a824d2
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-18 20:10:46 +0100

    task 0

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@
 int _printf(const char *format, ...)
 {
 	c_specifier spec[] = {
-		{"s", put_str}, {"c", put_char},
+		{"%s", put_str}, {"%c", put_char},
 		{"%i", put_int}, {"%d", put_dec}, {"%r", put_str_rev},
 		{"%R", put_r13}, {"%b", put_bin}, {"%u", put_unsign},
 		{"%o", put_oct}, {"%x", put_hex2}, {"%X", put_hex},
@@ -58,8 +58,8 @@ int _printfs(const char *format, ...)
 	int m, count = 0;
 
 	PrintDescriptor data[] = {
-		{"c", put_char},
-		{"s", put_str}
+		{"%c", put_char},
+		{"%s", put_str}
 	};
 	va_start(my_args, format);
 	if (!format)
