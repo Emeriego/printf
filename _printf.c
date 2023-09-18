0de7a8e1e41a5071eaf00721e86d76570ba1feb6
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-18 18:53:06 +0100

    updated.c

--- _printf.c
+++ _printf.c
@@ -1,50 +1,45 @@
 #include "main.h"
-
 /**
- * _printf - Custom printf function
- * @format: The format string
- *
- * Return: The number of characters printed
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
 int _printf(const char *format, ...)
 {
 	c_specifier spec[] = {
-		{"%s", put_str},
-		{"%c", put_char}
+		{"%s", put_str7}, {"%c", put_char7},
+		{"%i", put_int}, {"%d", put_dec}, {"%r", put_str_rev},
+		{"%R", put_r13}, {"%b", put_bin}, {"%u", put_unsign},
+		{"%o", put_oct}, {"%x", put_hex2}, {"%X", put_hex},
+		{"%S", put_str_ex}, {"%p", put_ptr2}, {"%%", put_percent7}
 	};
 
 	va_list my_args;
-	int w;
-	int spec_found;
-	int m, count;
+	int q = 0, w, spec_found, l = 0;
 
-	count = 0;
 	va_start(my_args, format);
-	if (!format)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	m = 0;
-	while (format && *(format + m))
+	while (format[q] != '\0')
 	{
-		if (format[m] == '%' && (!format[m + 1] || format[m + 1] == ' '))
-			return (-1);
-
-		for (w = 0; w < 14; w++)
+		spec_found = 0;
+		for (w = 13; w >= 0; w--)
 		{
-			if (format[m] == '%' && format[m + 1] == spec[w].sp[1])
+			if (spec[w].sp[0] == format[q] && spec[w].sp[1] == format[q + 1])
 			{
-				count += spec[w].f_ptr(my_args);
-				m += 2;
+				l += spec[w].f_ptr(my_args);
+				q = q + 2;
 				spec_found = 1;
 				break;
 			}
 		}
 		if (!spec_found)
 		{
-			_putchar(format[m]);
-			count++;
-			m++;
+			_putchar(format[q]);
+			l++;
+			q++;
 		}
 	}
 	va_end(my_args);
-	return (count);
+	return (l);
 }
