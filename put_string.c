eb3de83a0e98bf742fc706b438ed6299d206370e
Emeriego <emeriego@gmail.com>
2023-09-20 00:55:48 +0100

    code overhaul

---
+++ put_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * put_str - Print a string
+ * @my_args: The argument list
+ *
+ * Return: The number of characters in the string (excluding the null byte)
+ */
+int put_string(va_list val)
+{
+	char *s;
+	int i, len;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+}
