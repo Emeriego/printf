d576646a441edffe61734ce37588714635242144
Emeriego <emeriego@gmail.com>
2023-09-20 01:32:00 +0100

    code overhaul

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
@@ -8,7 +7,7 @@
 int _printf(const char *format, ...)
 {
 	c_specifier spec[] = {
-		{"%s", put_str}, {"%c", put_char},
+		{"%s", put_string}, {"%c", put_char7},
 		{"%i", put_int}, {"%d", put_dec}, {"%r", put_str_rev},
 		{"%R", put_r13}, {"%b", put_bin}, {"%u", put_unsign},
 		{"%o", put_oct}, {"%x", put_hex2}, {"%X", put_hex},
@@ -16,81 +15,30 @@ int _printf(const char *format, ...)
 	};
 
 	va_list my_args;
-	int q = 0, w, spec_found, l = 0;
+	int i = 0, j, len = 0;
 
 	va_start(my_args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (format[q] != '\0')
+
+Here:
+	while (format[i] != '\0')
 	{
-		spec_found = 0;
-		for (w = 13; w >= 0; w--)
+		j = 13;
+		while (j >= 0)
 		{
-			if (spec[w].sp[0] == format[q] && spec[w].sp[1] == format[q + 1])
+			if (spec[j].sp[0] == format[i] && spec[j].sp[1] == format[i + 1])
 			{
-				l += spec[w].f_ptr(my_args);
-				q = q + 2;
-				spec_found = 1;
-				break;
+				len += spec[j].f(my_args);
+				i = i + 2;
+				goto Here;
 			}
+			j--;
 		}
-		if (!spec_found)
-		{
-			_putchar(format[q]);
-			l++;
-			q++;
-		}
+		_putchar(format[i]);
+		len++;
+		i++;
 	}
 	va_end(my_args);
-	return (l);
-}
-
-/**
- * _printfs - Custom printf function
- * @format: The format string
- *
- * Return: The number of characters printed
- */
-int _printfs(const char *format, ...)
-{
-	va_list my_args;
-	int m, count = 0;
-
-	c_specifier spec[] = {
-		{"c", put_char},
-		{"s", put_str}
-	};
-	va_start(my_args, format);
-	if (!format)
-		return (-1);
-	m = 0;
-	while (format && *(format + m))
-	{
-		if (format[m] == '%' && (!format[m + 1] || format[m + 1] == ' '))
-			return (-1);
-
-		else if (*(format + m) == '%' && *(format + m + 1) == 'c')
-		{
-			count += spec[0].f_ptr(my_args);
-			m++;
-		}
-		else if (*(format + m) == '%' && *(format + m + 1) == 's')
-		{
-			count += spec[1].f_ptr(my_args);
-			m++;
-		}
-		else if (*(format + m) == '%' && *(format + m + 1) == '%')
-		{
-			count += _putchar('%');
-			m++;
-		}
-		else
-		{
-			_putchar(*(format + m));
-			count++;
-		}
-		m++;
-	}
-	va_end(my_args);
-	return (count);
+	return (len);
 }
